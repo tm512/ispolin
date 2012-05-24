61d9b8273165bde63f66dbf294314c2367b65903
tm512 <elykdav@gmail.com>
2012-05-23 18:59:19 -0700

    Indicate that we're on the stable branch in the version string

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 isp_version="0.1"
 git_version=""
 
-if [ -n $(which git) ] && [ -d .git ] ; then git_version=".git-$(git rev-parse --short HEAD)" ; fi
+if [ -n $(which git) ] && [ -d .git ] ; then git_version=".stable-$(git rev-parse --short HEAD)" ; fi
 
 echo "#define ISP_VERSION \"$isp_version\"" > src/version.h
 echo "#define GIT_VERSION \"$git_version\"" >> src/version.h
