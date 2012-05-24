20fe79bd7d197ca7bbdbddd1edc47745c2dd3b14
tm512 <elykdav@gmail.com>
2012-05-23 19:32:02 -0700

    Use "dev" instead of "git" in the version string since it more accurately represents the purpose of the master branch

--- version.sh
+++ version.sh
@@ -3,7 +3,7 @@
 isp_version="0.2"
 git_version=""
 
-if [ -n $(which git) ] && [ -d .git ] ; then git_version=".git-$(git rev-parse --short HEAD)" ; fi
+if [ -n $(which git) ] && [ -d .git ] ; then git_version=".dev-$(git rev-parse --short HEAD)" ; fi
 
 echo "#define ISP_VERSION \"$isp_version\"" > src/version.h
 echo "#define GIT_VERSION \"$git_version\"" >> src/version.h
